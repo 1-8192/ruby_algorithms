00709d519cfc02921e43c5a0d9950b7a1c26132f
1-8192 <allegranzia@gmail.com>
2019-09-05 16:15:59 -0400

    got number converter to work on 1-10

--- number_word_problem.rb
+++ number_word_problem.rb
@@ -1,1 +1,22 @@
 # 5. given a number N where 1 billion > N > 0 whole numbers only # output the number in words # ie: 384765 output three hundred eighty four thousand seven hundred sixty five
+
+def number_to_word_converter(number)
+    single_digits = [
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten"
+    ]
+
+    single_digits[number - 1 ]
+end
+
+p number_to_word_converter(4)
+p number_to_word_converter(10)
+p number_to_word_converter(1)
