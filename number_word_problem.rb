eb52695b16c7deeb79d53c58c39c80540e6c2787
1-8192 <allegranzia@gmail.com>
2019-09-11 14:32:15 -0400

    works for < 10000

--- number_word_problem.rb
+++ number_word_problem.rb
@@ -35,7 +35,7 @@
 
 def number_to_word_less_than_thousand(number)
     if number < 20
-        @single_digits[number - 1 ]
+        @single_digits[number - 1]
     elsif number < 100 
         test = number.to_s
         @tens_digits[test[0].to_i - 2] + " " + @single_digits[test[1].to_i - 1]
@@ -53,20 +53,26 @@ def number_to_word_converter(number)
    if number < 1000 
      return number_to_word_less_than_thousand(number)
    elsif number < 10000
-     test = number.to_s 
-     @single_digits[test[0].to_i - 1] + " thousand " + number_to_word_less_than_thousand(test.split.unshift.join.to_i)
+     test = number.to_s.split('') 
+     thousand = test.shift  
+
+     @single_digits[thousand.to_i - 1] + " thousand " + number_to_word_converter(test.join('').to_i)
    end 
 end
 
-p number_to_word_converter(4)
-p number_to_word_converter(10)
-p number_to_word_converter(1)
-p number_to_word_converter(16)
-p number_to_word_converter(19)
-p number_to_word_converter(25)
-p number_to_word_converter(99)
-p number_to_word_converter(78)
-p number_to_word_converter(109)
-p number_to_word_converter(534)
-p number_to_word_converter(999)
+# Test suite
+
+# p number_to_word_converter(4)
+# p number_to_word_converter(10)
+# p number_to_word_converter(1)
+# p number_to_word_converter(16)
+# p number_to_word_converter(19)
+# p number_to_word_converter(25)
+# p number_to_word_converter(99)
+# p number_to_word_converter(78)
+# p number_to_word_converter(109)
+# p number_to_word_converter(534)
+# p number_to_word_converter(999)
 p number_to_word_converter(1007)
+p number_to_word_converter(1345)
+p number_to_word_converter(9999)
